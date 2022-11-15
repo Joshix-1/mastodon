a43d2078900c12fa6c848a26a5fbf985267f4805
Auri <me@aurieh.me>
2022-11-15 01:10:15 +0000

    Change status length limit to 8192 characters

--- spec/validators/status_length_validator_spec.rb
+++ spec/validators/status_length_validator_spec.rb
@@ -16,20 +16,20 @@ describe StatusLengthValidator do
       expect(status).not_to receive(:errors)
     end
 
-    it 'adds an error when content warning is over 500 characters' do
-      status = double(spoiler_text: 'a' * 520, text: '', errors: double(add: nil), local?: true, reblog?: false)
+    it 'adds an error when content warning is over 8192 characters' do
+      status = double(spoiler_text: 'a' * 8212, text: '', errors: double(add: nil), local?: true, reblog?: false)
       subject.validate(status)
       expect(status.errors).to have_received(:add)
     end
 
-    it 'adds an error when text is over 500 characters' do
-      status = double(spoiler_text: '', text: 'a' * 520, errors: double(add: nil), local?: true, reblog?: false)
+    it 'adds an error when text is over 8192 characters' do
+      status = double(spoiler_text: '', text: 'a' * 8212, errors: double(add: nil), local?: true, reblog?: false)
       subject.validate(status)
       expect(status.errors).to have_received(:add)
     end
 
-    it 'adds an error when text and content warning are over 500 characters total' do
-      status = double(spoiler_text: 'a' * 250, text: 'b' * 251, errors: double(add: nil), local?: true, reblog?: false)
+    it 'adds an error when text and content warning are over 8192 characters total' do
+      status = double(spoiler_text: 'a' * 4096, text: 'b' * 4097, errors: double(add: nil), local?: true, reblog?: false)
       subject.validate(status)
       expect(status.errors).to have_received(:add)
     end
@@ -51,7 +51,7 @@ describe StatusLengthValidator do
     end
 
     it 'does not count overly long URLs as 23 characters flat' do
-      text = "http://example.com/valid?#{'#foo?' * 1000}"
+      text = "http://example.com/valid?#{'#foo?' * 8192}"
       status = double(spoiler_text: '', text: text, errors: double(add: nil), local?: true, reblog?: false)
       subject.validate(status)
       expect(status.errors).to have_received(:add)
@@ -66,7 +66,7 @@ describe StatusLengthValidator do
     end
 
     it 'does count both parts of remote usernames for overly long domains' do
-      text   = "@alice@#{'b' * 500}.com"
+      text   = "@alice@#{'b' * 8192}.com"
       status = double(spoiler_text: '', text: text, errors: double(add: nil), local?: true, reblog?: false)
 
       subject.validate(status)
