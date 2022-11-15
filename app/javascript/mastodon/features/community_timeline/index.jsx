fae833af9ad6660294816fa67daf4beb17119625
Auri <me@aurieh.me>
2022-11-15 02:07:08 +0000

    Add option to hide bot posts on public timelines

--- app/javascript/mastodon/features/community_timeline/index.jsx
+++ app/javascript/mastodon/features/community_timeline/index.jsx
@@ -41,6 +41,7 @@ const mapStateToProps = (state, { columnId }) => {
 class CommunityTimeline extends PureComponent {
   static defaultProps = {
     onlyMedia: false,
+    hideBots: false,
   };
 
   static propTypes = {
@@ -51,6 +52,7 @@ class CommunityTimeline extends PureComponent {
     hasUnread: PropTypes.bool,
     multiColumn: PropTypes.bool,
     onlyMedia: PropTypes.bool,
+    hideBots: PropTypes.bool,
   };
 
   handlePin = () => {
@@ -138,11 +140,20 @@ class CommunityTimeline extends PureComponent {
           <ColumnSettingsContainer columnId={columnId} />
         </ColumnHeader>
 
-        <StatusListContainer
-          prepend={<DismissableBanner id='community_timeline'><FormattedMessage id='dismissable_banner.community_timeline' defaultMessage='These are the most recent public posts from people whose accounts are hosted by {domain}.' values={{ domain }} /></DismissableBanner>}
+        <DismissableBanner id='community_timeline'>
+          <FormattedMessage id='dismissable_banner.community_timeline' defaultMessage='These are the most recent public posts from people whose accounts are hosted by {domain}.' values={{ domain }} />
+          {' '}
+          <FormattedMessage
+            id='dismissable_banner.community_timeline.filter_options'
+            defaultMessage='Check the configuration menu for ways to customize what appears on this timeline.'
+          />
+        </DismissableBanner>
+
+        <StatusListContainer          
           trackScroll={!pinned}
           scrollKey={`community_timeline-${columnId}`}
           timelineId={`community${onlyMedia ? ':media' : ''}`}
+          columnId={columnId}
           onLoadMore={this.handleLoadMore}
           emptyMessage={<FormattedMessage id='empty_column.community' defaultMessage='The local timeline is empty. Write something publicly to get the ball rolling!' />}
           bindToDocument={!multiColumn}
