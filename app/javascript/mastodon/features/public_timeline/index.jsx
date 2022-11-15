32ae6dd9f9dd0d238d3b1688e78e59be4df0ff32
Auri <me@aurieh.me>
2022-11-15 07:22:49 +0000

    Add option to hide sensitive bot posts on public timelines

--- app/javascript/mastodon/features/public_timeline/index.jsx
+++ app/javascript/mastodon/features/public_timeline/index.jsx
@@ -48,8 +48,9 @@ class PublicTimeline extends PureComponent {
   static defaultProps = {
     onlyMedia: false,
     hideBots: false,
+    hideSensitiveBots: false,
   };
-
+i
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     intl: PropTypes.object.isRequired,
@@ -58,6 +59,7 @@ class PublicTimeline extends PureComponent {
     hasUnread: PropTypes.bool,
     onlyMedia: PropTypes.bool,
     hideBots: PropTypes.bool,
+    hideSensitiveBots: PropTypes.bool,
     onlyRemote: PropTypes.bool,
   };
 
