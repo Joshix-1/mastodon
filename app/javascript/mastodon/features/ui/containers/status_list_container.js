2091e70ffab76148964a5d93f66cc1ec5769d787
Joshix <joshix.dev@mailbox.org>
2023-07-06 17:00:00 +0000

    increase poll option count

--- app/javascript/mastodon/features/ui/containers/status_list_container.js
+++ app/javascript/mastodon/features/ui/containers/status_list_container.js
@@ -9,10 +9,20 @@ import StatusList from '../../../components/status_list';
 import { me } from '../../../initial_state';
 
 const makeGetStatusIds = (pending = false) => createSelector([
-  (state, { type }) => state.getIn(['settings', type], ImmutableMap()),
+  (state, { type }) => state.getIn(['settings', type.split(':')[0]], ImmutableMap()),
   (state, { type }) => state.getIn(['timelines', type, pending ? 'pendingItems' : 'items'], ImmutableList()),
+  (state, { column }) => {
+    const columns = state.getIn(['settings', 'columns']);
+    const index   = columns.findIndex(c => c.get('uuid') === column);
+    return [column, index, columns.get(index)];
+  },
   (state)           => state.get('statuses'),
-], (columnSettings, statusIds, statuses) => {
+], (timelineSettings, statusIds, [ column, columnIndex, columnSettings ], statuses) => {
+  const showsSettings =
+    (column && columnIndex > 0)
+    ? columnSettings.getIn(['params', 'shows'], ImmutableMap())
+    : timelineSettings.get('shows', ImmutableMap());
+
   return statusIds.filter(id => {
     if (id === null || id === 'inline-follow-suggestions') return true;
 
@@ -40,13 +50,13 @@ const makeMapStateToProps = () => {
   const getStatusIds = makeGetStatusIds();
   const getPendingStatusIds = makeGetStatusIds(true);
 
-  const mapStateToProps = (state, { timelineId }) => ({
-    statusIds: getStatusIds(state, { type: timelineId }),
+  const mapStateToProps = (state, { timelineId, columnId }) => ({
+    statusIds: getStatusIds(state, { type: timelineId, column: columnId }),
     lastId:    state.getIn(['timelines', timelineId, 'items'])?.last(),
     isLoading: state.getIn(['timelines', timelineId, 'isLoading'], true),
     isPartial: state.getIn(['timelines', timelineId, 'isPartial'], false),
     hasMore:   state.getIn(['timelines', timelineId, 'hasMore']),
-    numPending: getPendingStatusIds(state, { type: timelineId }).size,
+    numPending: getPendingStatusIds(state, { type: timelineId, column: columnId }).size,
   });
 
   return mapStateToProps;
